082b048480c83e801a7b1026d768aa6fdc0847cc
DhavalKotak <DhavalKotak@users.noreply.github.com>
2021-07-13 14:50:18 +0530

    link updated

--- src/components/nav.js
+++ src/components/nav.js
@@ -30,7 +30,7 @@ export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="/"><img src={logo} alt="logo"/></Navbar.Brand>
+        <Navbar.Brand href="/portfolio"><img src={logo} alt="logo"/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
